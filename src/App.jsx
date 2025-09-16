e38729c18c30d94c4a6d68f3f9f011ac9345ca11
Will <wshagen3@gmail.com>
2025-09-16 16:36:47 -0700

    What I have so far

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+    return (
+        <main>
+            <div className="pattern" />
+            <div className="wrapper" >
+                <header>
+
+                    <img src="/images/hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+    </main>
+  )
+}
+
+export default App
